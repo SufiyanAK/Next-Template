7f13a2a7f66cae0b4d9d9c48896778df6bbdfabf
SufiyanAK <sufiyankhattak14@gmail.com>
2025-10-08 14:29:36 +0500

    Add RecentProjectScreen and RecentProjectComponent for managing recent projects

---
+++ src/app/(AuthenticatedRoutes)/projects/recent/page.tsx
@@ -0,0 +1,14 @@
+import RecentProjectComponent from "@/screens/recent-project-screen";
+
+export const metadata = {
+    title: 'RPA | Recent Projects',
+    description: 'View and manage your recent projects'
+};
+
+const RecentProjectScreen = () => {
+    return (
+        <RecentProjectComponent />
+    )
+}
+
+export default RecentProjectScreen
